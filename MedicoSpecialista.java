9ffc2ef21fc16cc96cd3a3aedee93a9aaf776ee4
Arianna Siniscalchi <siniscalchi.arianna@gmail.com>
2026-06-22 16:47:58 +0200

    Create MedicoSpecialista class extending Medico

---
+++ MedicoSpecialista.java
@@ -0,0 +1,13 @@
+package simulazione_TDE_2;
+
+public class MedicoSpecialista extends Medico implements Specialista {
+
+    public MedicoSpecialista(String idAlbo, String nome, String cognome, String specializzazione) {
+        super(idAlbo, nome, cognome, specializzazione);
+    }
+    
+    @Override
+    public int getNumeroPazientiAssegnati() {
+        return this.pazientiAssegnati.size(); 
+    }
+}
