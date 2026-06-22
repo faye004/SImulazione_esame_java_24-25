a1a8f714ba808a75c782572b9c3e25b8fb306f66
Arianna Siniscalchi <siniscalchi.arianna@gmail.com>
2026-06-22 16:47:29 +0200

    Create MedicoDiBase class extending Medico

---
+++ MedicoDiBase.java
@@ -0,0 +1,16 @@
+package simulazione_TDE_2;
+
+public class MedicoDiBase extends Medico {
+    
+    public MedicoDiBase(String idAlbo, String nome, String cognome) {
+        super(idAlbo, nome, cognome, "Medicina Generale");
+    }
+
+    /**
+     * Overriding: restituisce il numero reale di pazienti nel proprio pannello.
+     */
+    @Override
+    public int getNumeroPazientiAssegnati() {
+        return this.pazientiAssegnati.size();
+    }
+}
